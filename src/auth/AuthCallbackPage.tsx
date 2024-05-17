a0fde3157e969c96396bf78d13c8990f8672856f
BidhanCR <bidhanroy300805@gmail.com>
2024-05-17 07:18:06 +0600

    first commit

---
+++ src/auth/AuthCallbackPage.tsx
@@ -0,0 +1,26 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
+import { useAuth0 } from "@auth0/auth0-react"
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+
+const AuthCallbackPage = () => {
+    const navigate = useNavigate();
+    const hasCreateUser = useRef(false);
+    const {user} = useAuth0();
+    const {createUser} = useCreateMyUser();
+
+    useEffect(()=> {
+        if (user?.sub && user?.email && !hasCreateUser.current) {
+            createUser({ auth0Id: user.sub, email: user.email });
+            hasCreateUser.current = true;
+          }
+          navigate("/");
+    }, [createUser, navigate, user])
+  return (
+    <>Loading...</>
+  )
+}
+
+export default AuthCallbackPage
